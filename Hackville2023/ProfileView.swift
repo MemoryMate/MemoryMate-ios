c97ea2349a245b808561d142d7a521337868d06d
Roop Malhans <roopkm13@gmail.com>
2023-02-11 14:08:09 -0500

    MemoryMate- Login Page(Allows login with credentials)

---
+++ Hackville2023/ProfileView.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileView.swift
+//  Hackville2023
+//
+//  Created by Roop Malhans on 2023-02-11.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State var username:String = ""
+    @State var password:String = ""
+
+    var body: some View {
+        VStack{
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 170,height: 170)
+                .clipped()
+                .cornerRadius(150)
+                .padding(.bottom,75)
+            TextField("Username", text:$username)
+                .padding().background(Color(.systemGray6))
+                .cornerRadius(5.0)
+                .frame(width:350)
+                .padding(.bottom,20)
+            
+            SecureField("Password", text:$password)
+                .padding().background(Color(.systemGray6))
+                .cornerRadius(5.0)
+                .frame(width:350)
+                .padding(.bottom,20)
+            
+            Button(action: {
+                
+            }, label: {
+                Text("LOGIN")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width:220,height: 60)
+                    .background(.black)
+                    .cornerRadius(5.0)
+            })
+         
+            
+      
+
+            
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
