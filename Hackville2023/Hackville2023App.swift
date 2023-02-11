0c821725964d623e5b8d213ef1535ccf054cb3a1
Roop Malhans <roopkm13@gmail.com>
2023-02-11 12:43:55 -0500

    Initial Commit

---
+++ Hackville2023/Hackville2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Hackville2023App.swift
+//  Hackville2023
+//
+//  Created by Roop Malhans on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct Hackville2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
